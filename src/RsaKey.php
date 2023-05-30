5399b29b5d0282f1017dcc659ddee6f271a95ad0
Guillaume MODARD <guillaumemodard@gmail.com>
2023-05-30 12:58:52 +0200

    Debug

--- src/RsaKey.php
+++ src/RsaKey.php
@@ -54,6 +54,7 @@ class RsaKey implements RsaKeyInterface {
             $key = self::getKeyFileContent($filename);
         }
         catch (KeyNotFoundException $e) {
+            var_dump($filename);
             throw new PublicKeyNotFoundException($this->key);
         }
 
